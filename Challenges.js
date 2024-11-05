d166f9cacde7eb1c50e23fde744ac54bd4f9397b
deWasbeer <windhoos@gmail.com>
2024-11-05 12:36:28 +0100

    Update Challenges.js

--- Challenges.js
+++ Challenges.js
@@ -1,7 +1,7 @@
 // Hack Challanges
 
 // Start challenge : Klik op 'Start challenge'
-// open console (ctrl + alt + i) en type 'allow pasting' en druk enter
+// open console (ctrl + shift + i) en type 'allow pasting' en druk enter
 // plak ondrerstaande code in de console n druk op enter
 const exitButton = document.querySelector('.memo_exit_btn');
 
